6b1f044c2643ff5c7bf49a77dd489e2b5458b397
luojia65 <me@luojia.cc>
2022-02-01 15:09:01 +0800

    zihai: start boot hart

--- zihai/src/main.rs
+++ zihai/src/main.rs
@@ -1,4 +1,4 @@
-#![feature(asm_sym)]
+#![feature(asm_sym, asm_const, naked_functions)]
 #![no_std]
 #![no_main]
 
@@ -6,10 +6,10 @@ use core::arch::asm;
 use core::mem::MaybeUninit;
 
 // boot hart start
-pub extern "C" fn rust_init() {
+pub extern "C" fn rust_init(hartid: usize, opaque: usize) {
     // boot hart init
     unsafe {
-        asm!("li a7, 0x01", "mv a6, {}", "ecall", in(reg) b'R');
+        asm!("li a7, 0x01", "mv a6, {}", "ecall", in(reg) hartid + b'A');
     }
     // call sbi remote retentive suspension, use sbi 0.3 to wake other harts
 }
@@ -26,20 +26,24 @@ fn on_panic(_: &core::panic::PanicInfo) -> ! {
 const BOOT_STACK_SIZE: usize = 64 * 1024; // 64KB
 static BOOT_STACK: MaybeUninit<[u8; BOOT_STACK_SIZE]> = MaybeUninit::uninit();
 
+#[link_section = ".text.entry"]
 #[export_name = "_start"]
-pub unsafe extern "C" fn start() {
+#[naked]
+pub unsafe extern "C" fn start() -> ! {
     asm!(
         "beqz   a0, 2f",
         "1:",
         "wfi",
         "j      1b", // non-boot hart, halt
         "2:",
-        "mv     sp, {}",
-        "add    sp, sp, {}",
+        "la     sp, {}",
+        "li     t0, {}",
+        "add    sp, sp, t0",
         "call   {}",
         "unimp", // unreachable
-        in(reg) &BOOT_STACK,
-        in(reg) BOOT_STACK_SIZE,
+        sym BOOT_STACK,
+        const BOOT_STACK_SIZE,
         sym rust_init,
+        options(noreturn)
     )
 }
