e9312128339d6aa4232fba5944eca68505cdaedd
luojia65 <me@luojia.cc>
2022-02-03 13:44:04 +0800

    zihai: detect and use HSM to suspend non-boot harts
    small fix

--- zihai/src/main.rs
+++ zihai/src/main.rs
@@ -2,26 +2,39 @@
 #![no_std]
 #![no_main]
 
+#[macro_use]
+mod console;
+mod mm;
+mod sbi;
+
 use core::arch::asm;
 use core::mem::MaybeUninit;
 
 // boot hart start
-pub extern "C" fn rust_init(hartid: usize, _opaque: usize) {
+pub extern "C" fn rust_init(hartid: usize, opaque: usize) {
     // boot hart init
-    unsafe {
-        asm!("li a7, 0x01", "mv a6, {}", "ecall", in(reg) hartid + b'A' as usize);
+    println!("Welcome to zihai hypervisor");
+    let hsm_version = sbi::probe_extension(0x48534D);
+    if hsm_version == 0 { // HSM does not exist under current SBI environment
+        panic!("no HSM extension exist under current SBI environment");
     }
+    println!("Init hart id: {}", hartid);
+    println!("Opaque register: {}", opaque);
+    println!("SBI HSM probe identifier: {}", hsm_version);
+
     // call sbi remote retentive suspension, use sbi 0.3 to wake other harts
-    unsafe { asm!("li a7, 0x08", "ecall") }; // shutdown // todo: remove
+
+    sbi::reset(0x00000000, 0x00000000); // shutdown // todo: remove
 }
 
-pub extern "C" fn rust_init_harts() {
+pub extern "C" fn rust_init_harts(_opaque: usize) {
     // join working queue, ...
 }
 
 #[panic_handler]
-fn on_panic(_: &core::panic::PanicInfo) -> ! {
-    loop {}
+fn on_panic(info: &core::panic::PanicInfo) -> ! {
+    println!("{}", info);
+    sbi::reset(0x00000000, 0x00000001)
 }
 
 const BOOT_STACK_SIZE: usize = 64 * 1024; // 64KB
@@ -32,14 +45,27 @@ static BOOT_STACK: MaybeUninit<[u8; BOOT_STACK_SIZE]> = MaybeUninit::uninit();
 #[naked]
 pub unsafe extern "C" fn start() -> ! {
     asm!(
-        // start boot hart only
+        // prepare stack
+        "la     sp, {boot_stack}",
+        "li     t2, {boot_stack_size}",
+        "addi   t3, a0, 1",
+        "mul    t2, t2, t3",
+        "add    sp, sp, t2",
+        // start boot hart
         "beqz   a0, 2f",
-        "1:",
-        "wfi",
-        "j      1b", // non-boot hart, halt
-        "2:",
         "mv     t0, a0",
         "mv     t1, a1",
+        // stop other harts
+        "li     a7, 0x48534D",
+        "li     a6, 0x3", // hart suspend
+        "li     a0, 0x80000000",  // suspend type: non retentive
+        "la     a1, {rust_init_harts}", // resume address
+        "mv     a2, t1", // a2: opaque parameter
+        "ecall", // SBI hart syspend
+        "1:",
+        "wfi", // suspend failed, use WFI-loop halt instead
+        "j      1b", // non-boot hart, halt
+        "2:",
         // detect SBI version
         "li     a7, 0x10", // function id
         "li     a6, 0x0", // get spec version
@@ -48,16 +74,10 @@ pub unsafe extern "C" fn start() -> ! {
         "blt    a1, t2, 1f", // must >= SBI version 0.3
         "j      2f",
         "1:",
-        "la     sp, {boot_stack}",
-        "li     t2, {boot_stack_size}",
-        "add    sp, sp, t2",
         "mv     a0, a1",
         "tail   {err_sbi_version}",
         "unimp",
         "2:",
-        "la     sp, {boot_stack}",
-        "li     t2, {boot_stack_size}",
-        "add    sp, sp, t2",
         "mv     a0, t0",
         "mv     a1, t1",
         "tail   {rust_init}",
@@ -65,6 +85,7 @@ pub unsafe extern "C" fn start() -> ! {
         boot_stack = sym BOOT_STACK,
         boot_stack_size = const BOOT_STACK_SIZE,
         rust_init = sym rust_init,
+        rust_init_harts = sym rust_init_harts,
         err_sbi_version = sym err_sbi_version,
         options(noreturn)
     )
